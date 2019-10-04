accad8a2ca700f5e86171f05146655bbf4e777a5
swyoo1121 <tonyyoo0304@gmail.com>
2019-10-04 15:27:49 +0900

    Algorithm modified.

--- factorial.c
+++ factorial.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
 int main() {
+	int i;
 	int num = 6;
-	int product = 720;
+	int product = 1;
+	
+	for (i = 1 ; i <= num; i++) product *= i;
+	
 	printf("Factorial of %d is %d\n", num, product);
 }
